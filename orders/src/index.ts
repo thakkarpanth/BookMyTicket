6d194122050200ceb60837e224b0665709145892
thakkarpanth <panth.thakkar@actonate.com>
2021-05-31 17:36:11 +0530

    feat(Order Service):Created Order Service

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -20,11 +20,10 @@ const start = async () => {
   }
 
   try {
-
     await natsWrapper.connect(
-      process.env.NATS_CLUSTER_ID || '',
-      process.env.NATS_CLIENT_ID || '',
-      process.env.NATS_URL || ''
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
     );
     natsWrapper.client.on('close', () => {
       console.log('NATS connection closed!');
